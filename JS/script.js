1a3f09220c3d8733bdb0ac23ae84b5c001f277e8
Noel Injaga <injaganoel@gmail.com>
2021-03-07 01:05:50 +0300

    edited the css style sheet

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+$('.item1').click(function(){
+  $(".col-md-4").toggle();
+});
+$('.item2').click(function(){
+  $("#t1,#t2,#t3,#t4,#t5,#t6,#t7").toggle();
+});
+$('.item3').click(function(){
+  $("col-md-3").toggle();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
